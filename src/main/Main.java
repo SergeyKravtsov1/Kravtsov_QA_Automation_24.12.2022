57e2384d15d4d2e17db6d8d66e8c18356086f92a
Sergey Kravtsov <serhiikravtsovkharkiv@gmail.com>
2022-12-24 12:54:45 +0200

    Super

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,8 @@
 package main;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
+            System.out.println("Homework");
             System.out.println("Homework");
             System.out.println("Homework");
             System.out.println("Homework");
