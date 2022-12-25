b02098efb73f0cf15204b60928ab80186351a635
Sergey Kravtsov <serhiikravtsovkharkiv@gmail.com>
2022-12-25 15:00:09 +0200

    Without idea

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,8 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Homework");
-            System.out.println("Homework");
-            System.out.println("Homework");
-            System.out.println("Homework");
+            System.out.println("Without idea");
+
 
     }
 
